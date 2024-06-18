5c60f480480fbbda5671f038a6b9eea52b68fe0b
Pxlarized <chriz@liz.com>
2024-06-18 08:20:20 -0600

    Cambio en las variables de entorno

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import empresaAuth from './routes/auth/empresas/empresa.route'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express();
+
+dotenv.config()
+//Habilitar cors
 app.use(cors())
+//Leer datos de formularios
 app.use(express.json());
+
+
 const port = process.env.PORT || 3000;
 
-app.use("/", (req,res)=> {
-  res.send("Hola Mundo")
-})
 app.use("/api/auth/empresa", empresaAuth)
 
 app.listen(port, () => {
